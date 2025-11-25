544d43ed7d7edbe0e1c96ee3014578d7b80934b5
msyzzz <1258647557@qq.com>
2025-11-25 13:44:28 +0800

    update

--- imagecnn_512.py
+++ imagecnn_512.py
@@ -14,8 +14,7 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, roc_auc_score, roc_curve
 import matplotlib.pyplot as plt
 # os.environ['CUDA_VISIBLE_DEVICES'] = '2'
-expno = "hsratio"
-
+expno = "ttttttt"
 
 def printlog(s):
     print(s)
@@ -23,8 +22,6 @@ def printlog(s):
         f.write(s+"\n")
 
 
-
-# 定义CNN模型
 class CNN_16bit_Grayscale(nn.Module):
     def __init__(self):
         super(CNN_16bit_Grayscale, self).__init__()
@@ -37,8 +34,8 @@ class CNN_16bit_Grayscale(nn.Module):
         # self.dilated_conv = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
         # self.dilated_conv2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
         
-        self.fc1 = nn.Linear(128 * 64 * 64, 512)  # 输入特征图展平后的维度
-        self.fc2 = nn.Linear(512, 1)  # 二分类输出
+        self.fc1 = nn.Linear(128 * 64 * 64, 512)
+        self.fc2 = nn.Linear(512, 1)
         
         self.dropout = nn.Dropout(0.5)
 
@@ -73,8 +70,7 @@ class FocalLoss(nn.Module):
         focal_loss = self.alpha * ((1 - pt) ** self.gamma) * BCE_loss
         return focal_loss.mean()
 
-# 假设已经定义好数据集
-# 自定义数据集类（需要根据你自己的数据格式进行调整）
+
 class GrayscaleDataset(Dataset):
     def __init__(self, image_paths, labels, transform=None):
         self.image_paths = image_paths
@@ -93,10 +89,9 @@ class GrayscaleDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         # return image, label
-        return image, label, image_path  # 返回路径
+        return image, label, image_path 
 
     def load_image(self, path):
-        # 这里使用PIL库来加载图像，可以根据需要修改为加载16位灰度图
         image = Image.open(path).convert('I')  # 'I' 表示16位灰度图
         image = np.array(image)/65536
         return image.astype(np.float32)
@@ -109,7 +104,7 @@ class HardNegativeSampler(Sampler):
     def __init__(self, dataset, hard_sample_ratio=0.2):
         self.dataset = dataset
         self.hard_sample_ratio = hard_sample_ratio
-        self.hard_samples = set()  # 存储路径字符串
+        self.hard_samples = set() 
 
     def update(self, misclassified_paths):
         self.hard_samples.update(misclassified_paths)
@@ -137,106 +132,67 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5], std=[0.5])  # 假设16位灰度图范围在0-65535，可以根据实际情况调整
 ])
-shells = pd.read_csv("./data/LookPack-master/mal2020_shells.csv")
-be_shells = pd.read_csv("./data/LookPack-master/be2020_shells.csv")
-path_b = "./data/image_be2020_512"
-path_m = "./data/image_mal2020_512"
-path_m1 = "/Data2/masy/apk/image_mal2020_512"
 
-shells2 = pd.read_csv("./data/LookPack-master/mal2022_shells.csv")
-be_shells2 = pd.read_csv("./data/LookPack-master/be2022_shells.csv")
-path_b2 = "./data/image_be2022_512"
-path_m2 = "./data/image_mal2022_512"
-path_m12 = "/Data2/masy/apk/image_mal2022_512"
-path_be_jiagu = "./data/image_be_jiagu_512"
-path_be_jiagu_unpack = "/Data2/masy/apk/image_be_jiagu_512"
-path_jiagu = "/Data2/masy/apk/image_jiagu"
 image_paths = []
-be_selected = []
+labels = []
+exp = 2 
+benum = 0
+malnum = 0
+path_nb = "./data/image_non_packed_benign"
+path_nm = "./data/image_non_packed_malware"
+path_pb = "./data/image_packed_benign"
+path_pm = "./data/image_packed_malware"
+path_ub = "./data/image_unpacked_benign"
+path_um = "./data/image_unpacked_malware"
 
+for f in os.listdir(path_nb):
+        path = os.path.join(path_nb, f)
+        image_paths.append(path)
 
-files = os.listdir(path_b)
-#3914
-be_num = 3000
-count = 0
-for imagefile in files:
-    shell_item = be_shells[be_shells['sha256'] == imagefile.split('.')[0]]
-    # print(shell_item)
-    if shell_item['shell'].values != "unpackaged":
-        continue
-    image_paths.append(os.path.join(path_b ,imagefile))
-    be_selected.append(shell_item.iloc[0])
-    count += 1
-    if count == be_num:
-        break
-df_selected = pd.DataFrame(be_selected)
-df_selected.to_csv("be20_selected.csv", index=False)
+if exp == 1:
+    for f in os.listdir(path_pm):
+        path = os.path.join(path_pm, f)
+        image_paths.append(path)
+    benum = 6200
+    malnum = 5305
+if exp == 2:
+    for f in os.listdir(path_um):
+        path = os.path.join(path_um, f)
+        image_paths.append(path)
+    benum = 6200
+    malnum = 5305
+if exp == 3:
+    for f in os.listdir(path_pb):
+        path = os.path.join(path_pb, f)
+        image_paths.append(path)
+    for f in os.listdir(path_nm):
+        path = os.path.join(path_nm, f)
+        image_paths.append(path)
+    for f in os.listdir(path_pm):
+        path = os.path.join(path_pm, f)
+        image_paths.append(path)
+    benum = 6200 + 806
+    malnum = 5305 + 837
+if exp == 4:
+    for f in os.listdir(path_ub):
+        path = os.path.join(path_ub, f)
+        image_paths.append(path)
+    for f in os.listdir(path_nm):
+        path = os.path.join(path_nm, f)
+        image_paths.append(path)
+    for f in os.listdir(path_um):
+        path = os.path.join(path_um, f)
+        image_paths.append(path)
+    benum = 6200 + 208
+    malnum = 5305 + 837
+labels = [0.0] * benum + [1.0] * malnum
 
-
-files = os.listdir(path_b2)
-#3669
-be_num = 3200
-count = 0
-for imagefile in files:
-    shell_item = be_shells2[be_shells2['sha256'] == imagefile.split('.')[0]]
-    # print(shell_item)
-    if shell_item['shell'].values != "unpackaged":
-        continue
-    image_paths.append(os.path.join(path_b2 ,imagefile))
-    be_selected.append(shell_item.iloc[0])
-    count += 1
-    if count == be_num:
-        break
-
-df_selected = pd.DataFrame(be_selected)
-df_selected.to_csv("be22_selected.csv", index=False)
-
-files = os.listdir(path_be_jiagu_unpack)
-for imagefile in files:
-    image_paths.append(os.path.join(path_be_jiagu_unpack ,imagefile))
-    
-
-
-
-# files = os.listdir(path_jiagu)
-# for imagefile in files:
-#     image_paths.append(os.path.join(path_jiagu ,imagefile))
-
-files = os.listdir(path_m)
-for imagefile in files:
-    shell_item = shells[shells['sha256'] == imagefile.split('.')[0]]
-    # print(shell_item)
-    if shell_item['shell'].values == "unpackaged":
-        image_paths.append(os.path.join(path_m ,imagefile))
-files = os.listdir(path_m1)
-for imagefile in files:  
-    image_paths.append(os.path.join(path_m1 ,imagefile))
-    # image_paths.append(os.path.join(path_m ,imagefile))
-
-files = os.listdir(path_m2)
-for imagefile in files:
-    shell_item = shells2[shells2['sha256'] == imagefile.split('.')[0]]
-    # print(shell_item)
-    if shell_item['shell'].values == "unpackaged":
-        image_paths.append(os.path.join(path_m2 ,imagefile))
-files = os.listdir(path_m12)
-for imagefile in files:  
-    image_paths.append(os.path.join(path_m12 ,imagefile))
-    # image_paths.append(os.path.join(path_m2 ,imagefile))
-# 3. 数据准备
-# 假设你有图像路径列表 image_paths 和对应的标签列表 labels
-# image_paths = ["path_to_image1.jpg", "path_to_image2.jpg", "..."]  # 这里替换为你的图像路径
-
-# 725 2228 112 3077
-
-labels = [0.0] * (6200+208) + [1.0] * (5305+837)
-# 使用train_test_split划分训练集和验证集
 train_paths, val_paths, train_labels, val_labels = train_test_split(image_paths, labels, test_size=0.2, random_state=42)
 
 # 创建数据加载器
 train_dataset = GrayscaleDataset(train_paths, train_labels, transform)
 val_dataset = GrayscaleDataset(val_paths, val_labels, transform)
-sampler = HardNegativeSampler(train_dataset, hard_sample_ratio=1)
+sampler = HardNegativeSampler(train_dataset, hard_sample_ratio=0.4)
 train_loader = DataLoader(train_dataset, batch_size=64, num_workers=8, sampler=sampler)
 val_loader = DataLoader(val_dataset, batch_size=64, num_workers=8, shuffle=False)
 
@@ -250,11 +206,11 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.BCEWithLogitsLoss()
 
 # 训练过程
-num_epochs = 60  # 可以根据实际需要调整
+num_epochs = 60
 max_acc = 0
 
 for epoch in range(num_epochs):
-    model.train()  # 设置模型为训练模式
+    model.train()
     running_loss = 0.0
     correct = 0
     error_samples1 = []
@@ -262,12 +218,9 @@ for epoch in range(num_epochs):
     for images, labels,  paths in tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}'):
         images, labels = images.to(device), labels.to(device)
         
-        # 前向传播
         outputs = model(images)
-        
         loss = criterion(outputs.squeeze(dim=1), labels)
         
-        # 反向传播
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -275,10 +228,10 @@ for epoch in range(num_epochs):
         running_loss += loss.item()
 
         # predicted = torch.round(torch.sigmoid(outputs))
-        threshold = 0.7  # 举例：调高阈值，模型更保守，不轻易判正类
+        threshold = 0.7 
 
-        probs = torch.sigmoid(outputs)             # 转为概率
-        predicted = (probs >= threshold).int()     # 自定义阈值后分类
+        probs = torch.sigmoid(outputs)  
+        predicted = (probs >= threshold).int() 
         total += labels.size(0)
         correct += (predicted.squeeze() == labels).sum().item()
         for i in range(len(labels)):
@@ -289,14 +242,14 @@ for epoch in range(num_epochs):
     train_accuracy = 100 * correct / total
 
     # 验证过程
-    model.eval()  # 设置模型为评估模式
+    model.eval() 
     val_loss = 0.0
     correct = 0
     total = 0
     all_labels = []
     all_preds = []
     all_probs = []
-    error_samples = []  # 存储预测错误的样本路径
+    error_samples = []
     with torch.no_grad():
         for images, labels, paths in val_loader:
             images, labels = images.to(device), labels.to(device)
@@ -306,10 +259,10 @@ for epoch in range(num_epochs):
             val_loss += loss.item()
 
             # predicted = torch.round(torch.sigmoid(outputs))
-            threshold = 0.7  # 举例：调高阈值，模型更保守，不轻易判正类
+            threshold = 0.7
 
-            probs = torch.sigmoid(outputs)             # 转为概率
-            predicted = (probs >= threshold).int()     # 自定义阈值后分类
+            probs = torch.sigmoid(outputs) 
+            predicted = (probs >= threshold).int() 
             total += labels.size(0)
             correct += (predicted.squeeze() == labels).sum().item()
 
@@ -332,39 +285,36 @@ for epoch in range(num_epochs):
     cm = confusion_matrix(all_labels, all_preds)
     tn, fp, fn, tp = cm.ravel()
 
-    # 计算假阳性率（FPR）和假阴性率（FNR）
+
     fpr = fp / (fp + tn)  # False Positive Rate
     fnr = fn / (fn + tp)  # False Negative Rate
     recall = recall_score(all_preds, all_labels)
     precision = precision_score(all_preds, all_labels)
-    if val_accuracy > max_acc:
-        fprr, tprr, thresholds = roc_curve(all_labels, all_probs)
-        auc = roc_auc_score(all_labels, all_probs)
+    # if val_accuracy > max_acc:
+    #     fprr, tprr, thresholds = roc_curve(all_labels, all_probs)
+    #     auc = roc_auc_score(all_labels, all_probs)
 
-        # 绘图
-        plt.figure()
-        plt.plot(fprr, tprr, color='red', label=f'ROC curve (AUC = {auc:.4f})')
-        plt.plot([0, 1], [0, 1], color='gray', linestyle='--', label='Random (AUC = 0.5)')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('EXP3 ROC Curve')
-        plt.legend()
-        plt.grid()
-        # plt.show()
-        plt.savefig(f"./ROC/exp{expno}_epoch{epoch+1}.png")
-        plt.close()
-        max_acc = val_accuracy
-    # 打印每个epoch的训练和验证结果
+    #     # 绘图
+    #     plt.figure()
+    #     plt.plot(fprr, tprr, color='red', label=f'ROC curve (AUC = {auc:.4f})')
+    #     plt.plot([0, 1], [0, 1], color='gray', linestyle='--', label='Random (AUC = 0.5)')
+    #     plt.xlabel('False Positive Rate')
+    #     plt.ylabel('True Positive Rate')
+    #     plt.title('EXP3 ROC Curve')
+    #     plt.legend()
+    #     plt.grid()
+    #     # plt.show()
+    #     plt.savefig(f"./ROC/exp{expno}_epoch{epoch+1}.png")
+    #     plt.close()
+    #     max_acc = val_accuracy
+
     printlog(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%")
     printlog(f"Epoch [{epoch+1}/{num_epochs}], Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%")
 
     printlog(f"Epoch [{epoch+1}/{num_epochs}], FPR: {fpr:.4f}, FNR: {fnr:.4f}, Recall: {recall:.4f}, Precision: {precision:.4f}")
 
 
-
-# 获取当前时间
 now = datetime.now().strftime("%m_%d_%H_%M")
 
-# 模型保存
 torch.save(model.state_dict(), './model/16bit_{}.pth'.format(now))
 print("Model saved!")
